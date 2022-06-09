27e78786108857f2a07d32b99362d2d0c6ca2433
Aidan Prior <sifiwizard.code@gmail.com>
2022-06-08 19:29:55 -0600

    Movement

--- input.js
+++ input.js
@@ -1,14 +1,45 @@
-const setupInput = function () {
+
+let connection;
+
+const setupInput =  (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-
+  
+  let myInterval;
   const handleUserInput = function () {
     stdin.on('data', (key) => {
       if (key === '\u0003') {
+        console.log("Exited Program")
         process.exit();
       }
+      if (key === '\u0061') {
+        clearInterval(myInterval);
+        myInterval = setInterval(() => {
+          conn.write("Move: left")
+        }, 100);
+      }
+      if (key === '\u0077') {
+        clearInterval(myInterval);
+        myInterval = setInterval(() => {
+          conn.write("Move: up")
+        }, 100);
+      }
+      if (key === '\u0064') {
+        clearInterval(myInterval);
+        myInterval = setInterval(() => {
+          conn.write("Move: right")
+        }, 100);
+      }
+      if (key === '\u0073') {
+        clearInterval(myInterval);
+        myInterval = setInterval(() => {
+          conn.write("Move: down")
+        }, 100);      
+      }
+
     });
   };
 
